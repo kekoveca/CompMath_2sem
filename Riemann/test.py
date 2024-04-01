c49b0b202f6aecc2b3a4bd51f6eebcf48193493a
kekoveca <kulanov.av@phystech.edu>
2024-04-01 23:12:35 +0300

    update

--- Riemann/test.py
+++ Riemann/test.py
@@ -10,11 +10,11 @@ L = 10
 h = 0.1
 
 NX = int(2 * L / h + 1)
-print(NX)
+# print(NX)
 x_i = np.linspace(-L, L, int(NX))
-print(x_i)
+# print(x_i)
 
-print(NX // 2)
+# print(NX // 2)
 w_i = np.zeros((3, NX))
 
 for i in range(NX // 2):
@@ -25,7 +25,7 @@ for i in range((NX // 2), NX):
     w_i[0, i] = 1.3
     w_i[2, i] = 1 / (5 / 3 - 1)
 
-print(w_i)
+# print(w_i)
 v_i = np.zeros((3, NX))
 v_i[0, :] = w_i[0, :] 
 v_i[1, :] = w_i[1, :]
@@ -33,4 +33,5 @@ v_i[2, :] = w_i[2, :] / w_i[0, :]
 print(v_i[2, :].max())
 
 t = np.array([[-1, -1], [2, -1]])
-print(w_i[:, 1])
+print(w_i)
+print(w_i[:, 0])
